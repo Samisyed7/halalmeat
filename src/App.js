19598c4505cadc35cd7f3d818cb0360a8a15880e
shaikhMaksud <133562117+shaikhMaksud@users.noreply.github.com>
2023-08-17 13:26:59 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ function App() {
       <h1>sami syed</h1>
       <h1>zayn chaus</h1>
       <h1>This is ahtehsam </h1>
-<<<<<<<<< Temporary merge branch 1
-      <h1>Shaikh Maksud</h1>
-      <p>this is halal meat pro</p>
-=========
-      <h1>This is homepage</h1>
-      <h1>this is from ahtesham</h1>
-      <h>bablu</h>
-<<<<<<< HEAD
->>>>>>>>> Temporary merge branch 2
-=======
->>>>>>> 535f7f441d58c2c13fc9d97ffa96b73c80ac1320
+
     </div>
   );
 }
