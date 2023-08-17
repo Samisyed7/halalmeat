5c3f2581e5d1fcf0567e561d74f1fe2d771e253f
MohdAhteshamHasan <mohdahtesham2002@gmail.com>
2023-08-17 13:55:37 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { NavComp } from './Components/NavComp';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
