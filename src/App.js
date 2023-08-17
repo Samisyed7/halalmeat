d6a456dc2b9557e48127fc772bf25a748fcfd18d
MohdAhteshamHasan <mohdahtesham2002@gmail.com>
2023-08-17 14:32:18 +0530

    df

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import { Carasoul } from './Components/Carasoul';
-import { NavComp } from './Components/NavComp';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>sami syed</h1>
-      <h1>zayn chaus</h1>
-      <h1>This is ahtehsam </h1>
-<h1>Sayed Sami</h1>
-<p>akadlfl</p>
-<p>hey guys how are you </p>
-<NavComp/>
-<Carasoul/>
+
     </div>
   );
 }
