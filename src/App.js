f836a5a41a3b030ecb37adc1b60d785917353593
Samisyed7 <samisyed7888@gmail.com>
2023-08-17 15:05:04 +0530

    check

--- src/App.js
+++ src/App.js
@@ -7,12 +7,15 @@ import { NavComp } from './Components/NavComp';
 
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from './Components/card';
 
 function App() {
   return (
     <div className="App">
 
    <p>ahtehsam </p>
+   <NavComp/>
+   <Card/>
 
     </div>
   );
